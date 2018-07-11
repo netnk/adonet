829a084fe332d4326f16b053406a36f0399322f2
netnk <netnk0000@gmail.com>
2018-07-11 16:35:23 +0800

    first

---
+++ Models/TeamModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace adonet.Models
+{
+     public class Team
+    {
+        public int Team01 { get; set; }
+        public string Team02 { get; set; }
+        public string Team03 { get; set; }
+        public DateTime Team04 { get; set; }
+    }
+
+}
